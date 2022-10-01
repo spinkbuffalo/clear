4b545776bed491abf203fb1250cbf50925fba4b3
Stas <spinkbuffalo@gmail.com>
2022-10-01 21:16:40 +0300

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,9 @@
 import app from '../index.js';
 
 test('Positive cases ', () => {
-    expect(app('World')).toEqual('Hello World!');
+    expect(app('World')).toEqual('Hello, World!');
 });
 
 test('Negative cases ', () => {
-    expect(app()).toThrow('Name is required');
+    expect(() => app()).toThrow('Name is required');
 });
